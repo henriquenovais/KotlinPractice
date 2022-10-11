da1553f31721a1ccebf721b855de5a94d1490fa2
Henrique <henriquennoliveira@gmail.com>
2022-10-10 22:35:24 -0300

    Problem 2 level 1 Kotlin

--- app/src/main/java/com/example/beginner/Main.kt
+++ app/src/main/java/com/example/beginner/Main.kt
@@ -1,5 +1,19 @@
 package com.example.beginner
 
 fun main (args:Array<String>){
+    problem2_level1_extremely_basic(5,5)
+}
+
+fun problem1_level1_hello_world(){
     println("Hello World!")
+}
+
+fun problem2_level1_extremely_basic(a: Int, b: Int){
+    println("${a+b}")
+    val sc = Scanner(System.`in`);
+
+    val a: String = sc.next();
+    val b: String = sc.next();
+
+    println("X = " + (a.toInt() + b.toInt()));
 }
