c50220a1210562110bf7c5006d2c04a275b7d160
Henrique <henriquennoliveira@gmail.com>
2022-10-13 18:35:16 -0300

    Solve problem 4

--- app/src/main/java/com/example/beginner/ProblemsFirstLevel.kt
+++ app/src/main/java/com/example/beginner/ProblemsFirstLevel.kt
@@ -8,21 +8,26 @@ import kotlin.math.pow
 fun main (args:Array<String>){
     val sc = Scanner(System.`in`)
 
-    val r: String = sc.next()
-    //val b: String = sc.next()
+    val a: String = sc.next()
+    val b: String = sc.next()
 
-    problem3Level1AreaOfACircle(r)
+    problem4Level1SimpleSum(a,b)
 }
 
-fun problem1Level1HelloWorld(){
-    println("Hello World!")
-}
-
-fun problem2Level1ExtremelyBasic(a: String, b: String){
-    println("X = " + (a.toInt() + b.toInt()))
-}
-
-fun problem3Level1AreaOfACircle(r: String){
-    val result = BigDecimal(r.toDouble().pow(2)*3.14159).setScale(4, RoundingMode.HALF_EVEN)
-    println("A=${result}")
-}
+//fun problem1Level1HelloWorld(){
+//    println("Hello World!")
+//}
+//
+//fun problem2Level1ExtremelyBasic(a: String, b: String){
+//    println("X = " + (a.toInt() + b.toInt()))
+//}
+//
+//fun problem3Level1AreaOfACircle(r: String){
+//    val result = BigDecimal(r.toDouble().pow(2)*3.14159).setScale(4, RoundingMode.HALF_EVEN)
+//    println("A=${result}")
+//}
+//
+//fun problem4Level1SimpleSum(a: String, b: String){
+//    val result = a.toInt() + b.toInt()
+//    println("SOMA = ${result}")
+//}
